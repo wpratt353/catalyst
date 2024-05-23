4833a2ba1731d59a05130b5148a302a631baf231
Nathan Booker <nathan.booker@bigcommerce.com>
2024-05-23 12:29:21 -0400

    Enable PPR

--- core/next.config.js
+++ core/next.config.js
@@ -10,6 +10,7 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     optimizePackageImports: ['@icons-pack/react-simple-icons'],
+    ppr: true,
   },
   typescript: {
     ignoreBuildErrors: !!process.env.CI,
