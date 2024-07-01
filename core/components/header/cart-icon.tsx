7f7237dc3af3cc3ba138f3306a2c453d549cf613
Jorge Moya <jorge.moya@bigcommerce.com>
2024-07-01 14:16:01 -0500

    fix: cart-icon api

--- core/components/header/cart-icon.tsx
+++ core/components/header/cart-icon.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { ShoppingCart } from 'lucide-react';
+import { useLocale } from 'next-intl';
 import { useEffect, useState } from 'react';
 import { z } from 'zod';
 
@@ -18,9 +19,11 @@ export const CartIcon = ({ count }: CartIconProps) => {
   const [fetchedCount, setFetchedCount] = useState<number | null>();
   const computedCount = count ?? fetchedCount;
 
+  const locale = useLocale();
+
   useEffect(() => {
     async function fetchCartQuantity() {
-      const response = await fetch(`/api/cart-quantity/`);
+      const response = await fetch(`/api/cart-quantity/?locale=${locale}`);
       const parsedData = CartQuantityResponseSchema.parse(await response.json());
 
       setFetchedCount(parsedData.count);
@@ -32,7 +35,7 @@ export const CartIcon = ({ count }: CartIconProps) => {
     if (count === undefined) {
       void fetchCartQuantity();
     }
-  }, [count]);
+  }, [count, locale]);
 
   if (!computedCount) {
     return <ShoppingCart aria-label="cart" />;
