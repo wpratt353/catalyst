5ba82a077d7f3ab7d0b2640a9462ead345affdeb
Nathan Booker <nathan.booker@bigcommerce.com>
2024-08-15 13:01:27 -0500

    Simplify re-export of layout

--- core/app/catalyst/[locale]/layout.tsx
+++ core/app/catalyst/[locale]/layout.tsx
@@ -1,13 +1,5 @@
-import { PropsWithChildren } from 'react';
-
-import proxiedRootLayout, { generateMetadata, generateStaticParams } from '../../[locale]/layout';
+import RootLayout, { generateMetadata, generateStaticParams } from '../../[locale]/layout';
 
 export { generateMetadata, generateStaticParams };
 
-interface RootLayoutProps extends PropsWithChildren {
-  params: { locale: string };
-}
-
-export default function RootLayout({ children, params: { locale } }: RootLayoutProps) {
-  return proxiedRootLayout({ children, params: { locale } });
-}
+export default RootLayout;
