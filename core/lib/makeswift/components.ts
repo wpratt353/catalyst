8df32bd105512e8330bd1671c31cbadf8596b5ce
Matthew Volk <volkmattj@gmail.com>
2024-08-16 11:17:28 -0500

    feat: register featured image

--- core/lib/makeswift/components.ts
+++ core/lib/makeswift/components.ts
@@ -1,2 +1,3 @@
 import '~/components/ui/slideshow/slideshow.makeswift';
 import '~/components/ui/featured-products-carousel/featured-products-carousel.makeswift';
+import '~/components/ui/featured-image/featured-image.makeswift';
