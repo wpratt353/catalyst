835f2ac33ea61de33e602f7a9e7eea9adfa576b1
Matthew Volk <volkmattj@gmail.com>
2024-08-19 14:19:21 -0500

    refactor: use makeswift image control for images

--- core/components/ui/slideshow/slideshow.makeswift.tsx
+++ core/components/ui/slideshow/slideshow.makeswift.tsx
@@ -1,12 +1,38 @@
-import { List, Shape, Style, TextInput } from '@makeswift/runtime/controls';
-import { ComponentProps } from 'react';
+import { Image, List, Shape, Style, TextInput } from '@makeswift/runtime/controls';
 
 import { Slideshow } from '~/components/ui/slideshow';
 import { runtime } from '~/lib/makeswift/runtime';
 
+interface Props {
+  className?: string;
+  slides: Array<{
+    title: string;
+    description?: string;
+    image?: string;
+    imageAlt: string;
+    cta: {
+      label: string;
+      href: string;
+    };
+  }>;
+}
+
 runtime.registerComponent(
-  function MakeswiftSlideshow({ className, slides }: ComponentProps<typeof Slideshow>) {
-    return <Slideshow className={className} slides={slides} />;
+  function MakeswiftSlideshow({ className, slides }: Props) {
+    const transformedSlides = slides.map((slide) => ({
+      title: slide.title,
+      description: slide.description,
+      image: {
+        src: slide.image ?? '',
+        altText: slide.imageAlt,
+      },
+      cta: {
+        label: slide.cta.label,
+        href: slide.cta.href,
+      },
+    }));
+
+    return <Slideshow className={className} slides={transformedSlides} />;
   },
   {
     type: 'catalyst-slideshow',
@@ -19,13 +45,11 @@ runtime.registerComponent(
           type: {
             title: TextInput({ label: 'Title', defaultValue: '' }),
             description: TextInput({ label: 'Description' }),
-            image: Shape({
-              type: {
-                src: TextInput({ label: 'Image URL', defaultValue: '' }),
-                altText: TextInput({ label: 'Alt text', defaultValue: '' }),
-                blurDataUrl: TextInput({ label: 'Blur data URL' }),
-              },
+            image: Image({
+              label: 'Background image',
+              format: Image.Format.URL,
             }),
+            imageAlt: TextInput({ label: 'Image alt text', defaultValue: '' }),
             cta: Shape({
               type: {
                 label: TextInput({ label: 'Label', defaultValue: 'Shop now' }),
