d2836c65cdcd978c568dfa3a5b95cab8169fdb56
Matthew Volk <volkmattj@gmail.com>
2024-08-19 10:57:32 -0500

    feat: register newsletter subscription

--- core/lib/makeswift/components.ts
+++ core/lib/makeswift/components.ts
@@ -2,3 +2,4 @@ import '~/components/ui/slideshow/slideshow.makeswift';
 import '~/components/ui/featured-products-carousel/featured-products-carousel.makeswift';
 import '~/components/ui/featured-image/featured-image.makeswift';
 import '~/components/ui/featured-products-list/featured-product-list.makeswift';
+import '~/components/ui/subscribe/subscribe.makeswift';
